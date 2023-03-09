179fc222c9b84cf4d9cddce05ae4d4bd5c614bbe
Stangar999 <Stangar999@users.noreply.github.com>
2023-03-09 14:23:37 +0000

    deploy: 6e6004cbb7eef1049bc49630bce16c8b0c975b7e

---
+++ search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['submatrix_0',['SubMatrix',['../classMatrix_1_1SubMatrix.html',1,'Matrix']]],
+  ['subvalue_1',['SubValue',['../classMatrix_1_1SubValue.html',1,'Matrix']]]
+];
---
+++ search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hash_0',['Hash',['../structMatrix_1_1Index_1_1Hash.html',1,'Matrix::Index']]]
+];
