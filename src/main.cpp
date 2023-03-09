6e6004cbb7eef1049bc49630bce16c8b0c975b7e
Stangar999 <lockinout@mail.ru>
2023-03-09 17:05:13 +0300

    индексы храняться в set, а элементы матрицы линенйно хэш map

--- src/main.cpp
+++ src/main.cpp
@@ -29,27 +29,26 @@ int main() {
     std::cout << x << " " << y << " " << v << std::endl;
   }
 
-  //    Matrix<int, -1> matrix;
-  //    assert(matrix.size() == 0);
+  //  Matrix<int, -1> matrix;
+  //  assert(matrix.size() == 0);
 
-  //    auto a = matrix[0][0];
-  //    assert(a == -1);
-  //    assert(matrix.size() == 0);
+  //  auto a = matrix[0][0];
+  //  assert(a == -1);
+  //  assert(matrix.size() == 0);
 
-  //    matrix[100][100] = 314;
-  //    matrix[105][100] = 214;
-  //    matrix[108][100] = 114;
-  //    assert(matrix[100][100] == 314);
-  //    //assert(matrix.size() == 1);
+  //  matrix[100][100] = 314;
+  //  matrix[105][100] = 214;
+  //  matrix[108][100] = 114;
+  //  assert(matrix[100][100] == 314);
+  //  // assert(matrix.size() == 1);
 
-  //    for(auto c : matrix)
-  //    {
-  //        int x;
-  //        int y;
-  //        int v;
-  //        std::tie(x, y, v) = c;
-  //        std::cout << x << " " << y << " " << v << std::endl;
-  //    }
+  //  for (auto c : matrix) {
+  //    int x;
+  //    int y;
+  //    int v;
+  //    std::tie(x, y, v) = c;
+  //    std::cout << x << " " << y << " " << v << std::endl;
+  //  }
 
   cout << "Hello World! " << version() << endl;
   return 0;
--- src/matrix.h
+++ src/matrix.h
@@ -1,43 +1,52 @@
-﻿#include <unordered_map>
+﻿#include <set>
+#include <unordered_map>
 
 template <typename T, T default_val>
 class Matrix {
-  typedef std::unordered_map<size_t, T> VC;
-  std::unordered_map<size_t, VC> _container;
+  /// структура для объединения индексов строк и колонок в матрице
+  struct Index {
+    size_t col = 0;
+    size_t row = 0;
+    friend bool operator<(const Index& lth, const Index& rth) {
+      return std::tuple{lth.col, lth.row} < std::tuple{rth.col, rth.row};
+    }
+    friend bool operator==(const Index& lth, const Index& rth) {
+      return std::tuple{lth.col, lth.row} == std::tuple{rth.col, rth.row};
+    }
+    struct Hash {
+      size_t operator()(const Index& index) const {
+        return std::hash<size_t>{}(index.col) +
+               37ull * std::hash<size_t>{}(index.row);
+      }
+    };
+  };
+
+  /// контейнер самих значений по ключу Index
+  std::unordered_map<Index, T, typename Index::Hash> _container;
+  /// список индексов в отсортированом порядке для правильной итерации по
+  /// строкам
+  std::set<Index> _indexes;
 
  public:
-  Matrix(){};
-
-  int size() const {
-    size_t size = 0;
-    for (const auto& [key, vc] : _container) {
-      size += vc.size();
-    }
-    return size;
-  }
-
   class SubValue {
     Matrix& _matrix;
     T _value;
-    size_t _c;
-    size_t _r;
+    Index _indx;
 
    public:
     SubValue(Matrix& matrix, size_t c, size_t r)
         : _matrix(matrix),
-          _c(c),
-          _r(r),
+          _indx(Index{.col = c, .row = r}),
           _value(default_val) {
-      if (_matrix._container.count(c)) {
-        if (_matrix._container[c].count(r)) {
-          _value = _matrix._container[c][r];
-        }
+      if (_matrix._indexes.count(_indx)) {
+        _value = _matrix._container[_indx];
       }
     }
 
     SubValue& operator=(const T& t) {
       if (t != default_val) {
-        _matrix._container[_c][_r] = t;
+        _matrix._indexes.insert(_indx);
+        _matrix._container[_indx] = t;
       }
       return *this;
     }
@@ -60,63 +69,50 @@ class Matrix {
         : _matrix(matrix),
           _c(c){};
   };
+  template <typename It>
+  class Iterator : public std::iterator_traits<It> {
+    friend class Matrix;
+    Matrix& _matrix;
+    It _it;
+    Iterator(Matrix& matrix, It it)
+        : _matrix(matrix),
+          _it(it){};
+
+   public:
+    Iterator& operator++() {
+      if (_it == _matrix._indexes.end()) {
+        return *this;
+      }
+      ++_it;
+      return *this;
+    }
+
+    std::tuple<size_t, size_t, T> operator*() {
+      return {_it->col, _it->row, _matrix._container[*_it]};
+    }
+
+    bool operator!=(const Iterator& other) {
+      return other._it != _it;
+    }
+  };
+
+  Matrix(){};
+
+  size_t size() const {
+    size_t size = 0;
+    return _container.size();
+  }
 
   SubMatrix operator[](size_t c) {
     return {*this, c};
   }
 
-  // template<typename IT>
-  class Iterator /*: public std::iterator_traits<IT>*/ {
-    friend class Matrix;
-    Matrix& _matrix;
-    using It_c =
-        typename std::unordered_map<size_t,
-                                    std::unordered_map<size_t, int>>::iterator;
-    using It_r = typename std::unordered_map<size_t, int>::iterator;
-    It_c _it_c;
-    It_r _it_r;
-    Iterator(Matrix& matrix, It_c it_c)
-        : _matrix(matrix),
-          _it_c(it_c) {
-      if (_it_c != _matrix._container.end()) {
-        _it_r = _it_c->second.begin();
-      }
-    };
-
-   public:
-    Iterator& operator++() {
-      if (_it_c == _matrix._container.end()) {
-        return *this;
-      }
-      ++_it_r;
-      if (_it_r == _it_c->second.end()) {
-        ++_it_c;
-        if (_it_c == _matrix._container.end()) {
-          return *this;
-        }
-        _it_r = _it_c->second.begin();
-      }
-      return *this;
-    }
-
-    std::tuple<size_t, size_t, T> operator*() {
-      return {_it_c->first, _it_r->first, _it_r->second};
-    }
-
-    bool operator!=(Iterator other) {
-      if (other._it_c == _matrix._container.end() &&
-          _it_c == _matrix._container.end()) {
-        return false;
-      }
-      return other._it_c != _it_c || other._it_r != _it_r;
-    }
-  };
-
   auto begin() {
-    return Iterator(*this, _container.begin());
+    return Iterator<typename std::set<Index>::iterator>(*this,
+                                                        _indexes.begin());
   }
 
   auto end() {
-    return Iterator(*this, _container.end());
+    return Iterator<typename std::set<Index>::iterator>(*this, _indexes.end());
   }
 };
